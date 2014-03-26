7f3301a34872bd91d1e997509d2d2d74d93f86e7
stern <lilaboc.cn@gmail.com>
2014-03-26 22:41:41 +0800

    refactor problem 15

--- 15.rb
+++ 15.rb
@@ -1,34 +1,21 @@
 $cache = {}
 def bfs(a, b)
-  raise if a[0] > b[0] or a[1] > b[1]
-  #if (b[0] - a[0] == 1 and b[1] == a[1]) or ( b[1] - a[1] == 1 and a[0] == b[0] )
-  #  return 1
-  if a[0] == b[0]
-    if b[1] - a[1] == 1
-      1
-    else
-      $cache[[a[0], a[1] + 1]] = bfs([a[0], a[1] + 1], b) unless $cache.include?([a[0], a[1] + 1])
-      $cache[[a[0], a[1] + 1]]
-    end
-  elsif a[1] == b[1]
-    if b[0] - a[0] == 1
-      1
-    else
-      $cache[[a[0] + 1, a[1]]] = bfs([a[0] + 1, a[1]], b) unless $cache.include?([a[0] + 1, a[1]])
-      $cache[[a[0] + 1, a[1]]]
-    end
+  if (b[0] - a[0] == 1 and b[1] == a[1]) or ( b[1] - a[1] == 1 and a[0] == b[0] )
+    1
+  elsif a[0] == b[0]
+    getpoint([a[0], a[1] + 1], b)
+  elsif b[1] == a[1]
+    getpoint([a[0] + 1, a[1]], b)
   else
-
-    $cache[[a[0] + 1, a[1]]] = bfs([a[0] + 1, a[1]], b) unless $cache.include?([a[0] + 1, a[1]])
-    $cache[[a[0], a[1] + 1]] = bfs([a[0], a[1] + 1], b) unless $cache.include?([a[0], a[1] + 1])
-
-    #bfs([a[0] + 1, a[1]], b) + bfs([a[0], a[1] + 1], b)
-    $cache[[a[0] + 1, a[1]]] + $cache[[a[0], a[1] + 1]]
+    getpoint([a[0] + 1, a[1]], b) + getpoint(([a[0], a[1] + 1]), b)
   end
+end
+
+def getpoint(point, b)
+    $cache[point] = bfs(point, b) unless $cache.include?(point)
+    $cache[point]
 end
 
 
 p bfs([0, 0], [20, 20])
-#p bfs([0, 0], [1, 2])
-#p bfs([0, 0], [2, 2])
 
